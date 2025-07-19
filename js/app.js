33e6f1b57e559d72bcc68ddb05aa013071d24e39
Shukurullo <shukurulloshokiraliyev699@gmail.com>
2025-07-19 10:00:04 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,116 @@
+//1-masala
+
+// alert("1 dan 77 gacha bitta son o‘ylang ");
+// let topdim = false;
+// while (!topdim) {
+//   let taxmin = Math.floor(Math.random() * 77) + 1;
+//   let javob = prompt( + taxmin + " to'g'rimi? ");
+//   if (javob === "ha") {
+//     alert("  Topdim! ");
+//     topdim = true;
+//   } else if (javob === "yo‘q" || javob === "yoq") {
+//   } 
+// }
+
+//2-masala
+
+//1-qism
+// let son = prompt("haqiqiy son kirit")
+// console.log("floor", Math.floor(son));
+// console.log("ceil", Math.ceil(son));
+// console.log("round", Math.round(son));
+//2-qism
+// let random = Math.floor(Math.random() * 11);  
+// console.log("Random son:", random);
+
+//3-masala
+
+// let a;           
+// let n = null;   
+// console.log("a", a);  
+// console.log("n", n);  
+
+//4-masala
+
+// let count = 0;
+// while (count < 10) {
+//   let randomSon = Math.floor(Math.random() * 91) + 10;  
+//   console.log(randomSon);
+//   count++;
+// }
+
+//5-masala
+
+// let son = +prompt(" son kirit")
+// console.log(Math.pow(son,2));
+
+//6-masala
+
+// son=0
+// while (son < 20) {
+//   if (son % 2 === 0) {
+//     continue; 
+//   }else{
+//       console.log(son); 
+//   }
+// }
+
+//7-masala
+// const secret = Math.floor(Math.random() * 10) + 1;
+// let son;
+// do {
+//   son = (prompt("Son kiriting (1-10):"));
+//   if (son !== secret) {
+//     console.log("Xato. Yana kiriting.");
+//   }
+// } while (son !== secret);
+
+// console.log("To'g'ri topdingiz!", );
+
+//8-masala
+
+// let soncha=1;
+
+// while(soncha<50){
+//     if(soncha%3==0){
+//         if(soncha===21){
+//             break
+//         }
+//         console.log(soncha);
+//     }
+//     soncha++;
+// }
+// 9-misol
+
+// let son1 = Math.floor(Math.random() * 100);
+// let son2 = Math.floor(Math.random() * 100);
+// let son3 = Math.floor(Math.random() * 100);
+// let son4 = Math.floor(Math.random() * 100);
+// let son5 = Math.floor(Math.random() * 100);
+// console.log(son1, son2, son3, son4, son5);
+
+// if (son1 > son2 && son1 > son3 && son1 > son4 && son1 > son5) {
+//   console.log(son1);
+// } else if (son2 > son3 && son2 > son4 && son2 > son5) {
+//   console.log(ran2);
+// } else if (son3 > son4 && son3 > son4) {
+//   console.log(son3);
+// } else if (son4 > son5) {
+//   console.log(son4);
+// } else {
+//   console.log(son5);
+// }
+
+// 10-misol
+
+// let condition = true;
+// while (condition) {
+//   let number = +prompt("Son kiriting: ");
+
+//   if (isNaN(number)  number === undefined  number === null) {
+//     alert("Iltimos, son kiriting!");
+//   } else {
+//     alert("To'giri son kiritiz");
+//     condition = false;
+//   }
+// }
